676c8fe7611d3910f99f8fbfcbd3883e767bcb9e
Caleb Villalba <49006430+KirisuteG@users.noreply.github.com>
2020-02-25 19:39:14 -0500

    Update app.py
    sin boton done

--- app.py
+++ app.py
@@ -4,24 +4,22 @@ from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databases/database0.db'
 db = SQLAlchemy(app)
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(256))
-    done = db.Column(db.Boolean)
+
 
 @app.route('/chain', methods=['GET'])
 def home():
     tasks = Task.query.all() 
     return render_template('index.html', tasks = tasks)
 
-
 @app.route('/chain', methods=['POST'])
 def create():
-    task = Task(content=generate_password_hash(request.form['content']),done=False)  
+    task = Task(content=generate_password_hash(request.form['content']))  
     db.session.add(task)
     db.session.commit()
     return redirect(url_for('home'))
@@ -33,7 +31,13 @@ def last():
 
 @app.route('/api/v1/chain/', methods=['GET'])
 def json():
-    return jsonify({'Json': Task.query.all()})
+    tasks = Task.query.all()
+    dict = {}
+    j = 0
+    for i in tasks:
+        dict[j]=i
+        j+=1
+    return jsonify(dict)
 
 @app.route('/chain/done/<id>')
 def done(id):
@@ -49,6 +53,5 @@ def delete(id):
     return redirect(url_for('home'))
 
 
-
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)  
+    app.run(host='0.0.0.0', port=3000, debug=True)  
